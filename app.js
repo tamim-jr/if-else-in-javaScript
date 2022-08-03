39b0c82d6fdf352a051f6ca33fe90c042ebee0bd
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-03 23:28:23 +0600

    if else if and else example added

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 let number = 24;
 
-if (number == 24){
+if (number == 25){
     console.log("Yes!!!");
+}
+else{
+    console.log("No!!");
+};
+
+
+//proper example of if else
+let num = 50;
+if (num > 10){
+    console.log("The number is positive");
+}
+else if (num <0){
+    console.log("The number is negative");
+}
+else {
+    console.log("The number is equal");
 }
