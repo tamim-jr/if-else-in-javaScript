08fad8dc73854c1b8be91d2be0a5468c8851510c
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-06 00:07:26 +0600

    Voter age calculator in js

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-let number = 24;
+let number1 = 24;
 
-if (number == 25){
+if (number1 == 25) {
     console.log("Yes!!!");
 }
-else{
+else {
     console.log("No!!");
 };
 
 
 //proper example of if else
 let num = 50;
-if (num > 10){
+if (num > 10) {
     console.log("The number is positive");
 }
-else if (num <0){
+else if (num < 0) {
     console.log("The number is negative");
 }
 else {
     console.log("The number is equal");
+};
+
+//getter then equal
+
+let number = 18;
+
+if (number >= 18) {
+    console.log("You are a voter");
 }
+else {
+    console.log("You are not a voter");
+};
+
+
